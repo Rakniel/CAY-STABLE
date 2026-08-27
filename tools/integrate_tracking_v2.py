f3413c8169d03d2052335e1692dc7284a43d2151
Rakniel <team1dofus@gmail.com>
2026-08-27 09:21:38 +0200

    feat(stable): load metric quality guard

--- tools/integrate_tracking_v2.py
+++ tools/integrate_tracking_v2.py
@@ -15,6 +15,7 @@ canonical_tags = [
     '<script src="./metric_pitch_heatmap_v1.js"></script>',
     '<script src="./ball_event_state_v1.js"></script>',
     '<script src="./player_stats_v1.js"></script>',
+    '<script src="./metric_quality_guard_v1.js"></script>',
     '<script src="./stable_tracking_bridge_v1.js"></script>',
     '<script src="./strict_tracking_frame_guard_v1.js"></script>',
     '<script src="./manual_identity_merge_guard_v1.js"></script>',
