f8ff5f0c1b2fa273e58d456ced650a73c2258ed4
Rakniel <team1dofus@gmail.com>
2026-08-27 07:21:19 +0200

    feat(stable): load conservative ReID evidence module

--- tools/integrate_tracking_v2.py
+++ tools/integrate_tracking_v2.py
@@ -8,6 +8,7 @@ canonical_tags = [
     '<script src="./tracking_confidence_cascade_v1.js"></script>',
     '<script src="./tracking_two_stage_adapter_v1.js"></script>',
     '<script src="./tracking_two_stage_runtime_patch_v1.js"></script>',
+    '<script src="./reid_evidence_fusion_v1.js"></script>',
     '<script src="./metric_homography_projector_v1.js"></script>',
     '<script src="./metric_segment_registry_v1.js"></script>',
     '<script src="./metric_pitch_heatmap_v1.js"></script>',
